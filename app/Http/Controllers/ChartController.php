3f9d056038c3f117dbd112f95561877c9cfbf038
TaufanNurrizkie <opan0311@gmail.com>
2024-10-05 21:06:37 +0700

    ngebenerin chart dan tampilan lain

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -12,37 +12,42 @@ class ChartController extends Controller
     {
         // Contoh data statis
         $pieChart = LarapexChart::pieChart()
-            ->setTitle('')
+            ->setTitle('Persentase total realisasi berdasarkan sektor CSR')
             ->setDataset([44, 55, 13, 33])
             ->setLabels(['Sektor 1', 'Sektor 2', 'Sektor 3', 'Sektor 4']);
     
-        $barChart = LarapexChart::barChart()
-            ->setTitle('')
+        $barChart = LarapexChart::horizontalBarChart()
+            ->setTitle('Total realisasi sektor CSR')
             ->setDataset([
                 [
                     'name' => 'Proyek CSR',
-                    'data' => [30, 40, 45, 50, 49, 60, 70, 91, 125, 134, 145]
+                    'data' => [30, 40, 45, 50, 49, 60]
                 ]
             ])
-            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des']);
-        $barChart2 = LarapexChart::barChart()
-            ->setTitle('')
+            ->setColors(['#008FFB', '#00E396', '#feb019', '#ff455f', '#775dd0', '#80effe'])
+            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul']);
+
+        $barChart2 = LarapexChart::horizontalBarChart()
+            ->setTitle('Presentase total realisasi berdasarkan mitra')
             ->setDataset([
                 [
                     'name' => 'Proyek CSR',
-                    'data' => [30, 40, 45, 50, 49, 60, 70, 91, 125, 134, 145]
+                    'data' => [30, 40, 45, 50, 49, 60]
                 ]
             ])
-            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des']);
-        $barChart3 = LarapexChart::barChart()
-            ->setTitle('')
+            ->setColors(['#008FFB', '#00E396', '#feb019', '#ff455f', '#775dd0', '#80effe'])
+            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul']);
+            
+        $barChart3 = LarapexChart::horizontalBarChart()
+            ->setTitle('Presentase total realisasi berdasarkan mitra')
             ->setDataset([
                 [
                     'name' => 'Proyek CSR',
-                    'data' => [30, 40, 45, 50, 49, 60, 70, 91, 125, 134, 145]
+                    'data' => [30, 40, 45, 50, 49, 60]
                 ]
             ])
-            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des']);
+            ->setColors(['#008FFB', '#00E396', '#feb019', '#ff455f', '#775dd0', '#80effe'])
+            ->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul']);
 
             
     return view('admin.dashboard', compact('pieChart', 'barChart', 'barChart2', 'barChart3'));
