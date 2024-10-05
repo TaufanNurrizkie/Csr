c572efb6a47f9daadfa2c7816a69a245537ce577
TaufanNurrizkie <opan0311@gmail.com>
2024-10-05 09:54:29 +0700

    ubah route manajemen

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartController;
 use App\Http\Controllers\ReportController;
 
-Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
-Route::get('/reports/{id}', [ReportController::class, 'show'])->name('reports.show');
-Route::post('/reports/{id}/approve', [ReportController::class, 'approve'])->name('reports.approve');
-Route::post('/reports/{id}/reject', [ReportController::class, 'reject'])->name('reports.reject');
+// Route untuk Admin yang bisa mengakses halaman laporan CSR
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+    Route::get('/reports/{id}', [ReportController::class, 'show'])->name('reports.show');
+    Route::post('/reports/{id}/approve', [ReportController::class, 'approve'])->name('reports.approve');
+    Route::post('/reports/{id}/reject', [ReportController::class, 'reject'])->name('reports.reject');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
