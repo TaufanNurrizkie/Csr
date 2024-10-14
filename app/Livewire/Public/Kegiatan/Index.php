638d2e5cb341d815976a098acf42da88cc196bbb
fahrirxtak <nfahri765@gmail.com>
2024-10-14 15:51:25 +0700

    menambahkan folder public

---
+++ app/Livewire/Public/Kegiatan/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Public\Kegiatan;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.kegiatan.index');
+    }
+}
